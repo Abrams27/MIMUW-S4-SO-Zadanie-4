c4cd5b5d4865f1b6a52ff051fca274e6c4b08834
Abrams27 <abramowiczmarcin99@gmail.com>
2020-05-14 22:11:58 +0200

    testy?

--- minix_source/test/give_kudos_test.c
+++ minix_source/test/give_kudos_test.c
@@ -7,9 +7,52 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-int main() {
+int first_child_pid;
 
-  givekudos(2137);
+int main() {
+  printf("startowy pid: %d\n", getpid());
+  printf("liczba kudosow startowego: 0\n");
+
+  switch (fork()) {
+    case 0:
+      first_child_pid = getpid();
+      usleep(30000);
+
+      break;
+
+    default:
+      usleep(1000);
+
+      printf("probuje dac kudosy swojemu dziecku: nie powinno sie udać (moj pid: %d)\n", getpid());
+      givekudos(first_child_pid);
+      printf("nie udalo sie? sprawdz! (moj pid: %d)\n", getpid());
+
+      if (wait(0) == -1)
+        syserr("wait");
+
+      break;
+  }
+
+  switch (fork()) {
+    case 0:
+      usleep(10000);
+
+      printf("probuje dac kudosy swojemu kuzynowi: powinno sie udać (moj pid: %d)\n", getpid());
+      givekudos(first_child_pid);
+      printf("udalo sie? sprawdz! (moj pid: %d)\n", getpid());
+
+      usleep(30000);
+
+      break;
+
+    default:
+      usleep(1000);
+
+      if (wait(0) == -1)
+        syserr("wait");
+
+      break;
+  }
 
   return 0;
 }
